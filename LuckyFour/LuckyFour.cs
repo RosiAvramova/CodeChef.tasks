86bd2864dce16cdd107dfe859cc59f9bce2f0ad8
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-06 14:35:33 +0300

    Lucky Four

---
+++ LuckyFour/LuckyFour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LuckyFour
+{
+    internal class LuckyFour
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            int[] result = new int[testCases];
+            for (int i = 0; i < testCases; i++)
+            {
+                string input = Console.ReadLine();
+                int counter = 0;
+                foreach (var ch in input)
+                {
+                    if (ch =='4')
+                    {
+                        counter++;
+                    }
+                }
+                result[i] = counter;
+            }
+            Console.WriteLine(string.Join("\n",result));
+        }
+    }
+}
