d0443b2db168202d028885d9e2f1c2db16fd8f85
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-06 16:09:54 +0300

    Brute-Force

---
+++ Brute-Force/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Brute_Force
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            
+                while (input!="42")
+                {
+                    Console.WriteLine(input);
+                    input = Console.ReadLine();
+                }
+            
+        }
+    }
+}
