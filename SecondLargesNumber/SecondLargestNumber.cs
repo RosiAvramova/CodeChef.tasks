daa5b6a08e308b199c1db5c2ee27a8737208fcc6
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-06 15:11:32 +0300

    Second Largest Number

---
+++ SecondLargesNumber/SecondLargestNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecondLargesNumber
+{
+    internal class SecondLargestNumber
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            
+            int[] result = new int[testCases];
+            
+            for (int i = 0; i < testCases; i++)
+            {
+                string[] numbersAsStrings = Console.ReadLine().Split(' ');
+                int[] numbers = new int[numbersAsStrings.Length];
+                for (int j = 0; j < numbersAsStrings.Length; j++)
+                {
+                    numbers[j] = int.Parse(numbersAsStrings[j]);    
+                }
+                Array.Sort(numbers);
+                result[i] = numbers[numbers.Length-2];
+            }
+            Console.WriteLine(String.Join("\n",result));
+        }
+    }
+}
+    
+
