633538c066f4c623b603a40f7e5b87db49838b73
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-06 14:05:40 +0300

    Pass or Fail

---
+++ PassOrFail/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PassOrFail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            string[] result = new string[testCases];
+            for (int i = 0; i < testCases; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int numberOfQuestions = int.Parse(input[0]);
+                int correctAnswers = int.Parse(input[1]);
+                int minScore = int.Parse(input[2]);
+                int testResult = 0;
+                int wrongAnswers = numberOfQuestions - correctAnswers;
+                testResult = correctAnswers * 3;
+                testResult = testResult - wrongAnswers;
+                if (testResult>=minScore)
+                {
+                    result[i] = "PASS";
+                }
+                else
+                {
+                    result[i] = "FAIL";
+                }
+
+
+            }
+            Console.WriteLine(String.Join("\n",result));
+        }
+    }
+}
