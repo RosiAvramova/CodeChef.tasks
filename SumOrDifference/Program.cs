e3999cb8aa76e4bb2cd205336ed05489f38d96ec
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-06 15:21:41 +0300

    Sum Or Difference

---
+++ SumOrDifference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumOrDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int result = 0;
+            if (n1 > n2)
+            {
+                result = (n1 - n2);
+            }
+            else
+            {
+               result = ( n1 + n2);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
