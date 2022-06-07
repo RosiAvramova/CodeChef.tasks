88186944698b2cdf5a8e86ca056a67af87697c93
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-07 18:11:32 +0300

    Vacation

---
+++ AvoidContact/Vacation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvoidContact
+{
+    internal class Vacation
+    {
+        static void Main(string[] args)
+        {
+            int tastCases = int.Parse(Console.ReadLine());
+            List<string> result = new List<string>();
+            for (int i = 0; i < tastCases; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int familyTrip = int.Parse(input[0]);
+                int friendsTrip = int.Parse(input[1]);
+                int holiday = int.Parse(input[2]);
+                if (familyTrip + friendsTrip <= holiday)
+                {
+                    result.Add("YES");
+                }
+                else if (friendsTrip + familyTrip > holiday)
+                {
+                    result.Add("NO");
+                }
+            }
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
