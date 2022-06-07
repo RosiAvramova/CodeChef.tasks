3c3b624e15316b153b4f267f74a09a7d2cacc1cc
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-07 17:42:47 +0300

    car choice

---
+++ CarChoice/Car Choice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarChoice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int testCases = int.Parse(Console.ReadLine());
+            List<int> result = new List<int>();
+            for (int i = 0; i < testCases; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                double x1 = double.Parse(input[0]);
+                double x2 = double.Parse(input[1]);   
+                double y1 = double.Parse(input[2]);   
+                double y2 = double.Parse(input[3]);
+                if ((y1/x1)<(y2/x2))
+                {
+                    result.Add(-1);
+                }
+                else if ((y1/x1)>(y2/x2))
+                {
+                    result.Add(1);
+                }
+                else if ((y1/x1) == (y2/x2))
+                {
+                    result.Add(0);
+
+                }
+                
+                
+
+
+            }
+            Console.WriteLine(string.Join("\n",result));
+        }
+    }
+}
---
+++ TheBlockGame/The Block Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheBlockGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            List<string> result = new List<string>();
+            for (int i = 0; i < testCases; i++)
+            {
+                string number = Console.ReadLine();
+                char[] numberArr = number.ToCharArray();
+                Array.Reverse(numberArr);
+                string reversed = new string(numberArr);
+                if (number == reversed)
+                {
+                    result.Add("wins");
+                }
+                else
+                {
+                    result.Add("loses");
+                }
+            }
+            Console.WriteLine(string.Join("\n",result));
+
+        }
+    }
+}
